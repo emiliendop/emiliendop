1187056b48a5ad197422c2993c6ff65fc116d20d
emiliendop <emiliendop10@gmail.com>
2022-03-19 18:28:28 +0100

    nommage titre

--- information.php
+++ information.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>INFORMATION</title>
 </head>
 <body>
     <?php $connexion= new mysqli("localhost","root","","cuisine");
